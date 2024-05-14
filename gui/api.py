dc40fb8b597232fa8afb23ef23019a1a24f87dcd
sri0606 <srirams0606@gmail.com>
2024-05-14 15:21:40 -0400

    working app

--- gui/api.py
+++ gui/api.py
@@ -1,41 +1,99 @@
+import os
+import json
 from src.video import Video
+from src.captions import parse_captions
 
 class VidCaptioAPI:
     def __init__(self):
         self.video_captioner = Video()
+        self.are_captions_generated = False
+        self.captions_type="srt"
+
+    def update_json_file(self, new_data):
+        json_file_path = os.path.join(self.vid_folder, 'details.json')
+        if not os.path.isfile(json_file_path):
+            with open(json_file_path, 'w') as json_file:
+                json.dump(new_data, json_file)
+        else:
+            with open(json_file_path, 'r+') as json_file:
+                data = json.load(json_file)
+                data.update(new_data)
+                json_file.seek(0)
+                json.dump(data, json_file)
+                json_file.truncate()
 
     def set_video_data(self, vid_path,vid_folder,vid_extension):
         self.vid_path_original = vid_path
         self.vid_folder = vid_folder
         self.vid_extension = vid_extension
     
-    def generate_captions(self,source_lang: str, dest_langs: list):
+    def load_project_data(self,vid_folder):
+        json_file_path = os.path.join(vid_folder, 'details.json')
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
+            self.vid_path_original = data['vid_path_original']
+            self.vid_folder = data['vid_folder']
+            self.vid_extension = data['vid_extension']
+            self.vid_language = data['vid_language']
+            self.dest_languages = data['dest_languages']
+            self.are_captions_generated = data['are_captions_generated']
+            self.captions_type = data['captions_type']
+
+    def save_project_data(self):
+        new_data = {
+            'vid_path_original': self.vid_path_original,
+            'vid_folder': self.vid_folder,
+            'vid_extension': self.vid_extension,
+            'vid_language': self.vid_language if hasattr(self, 'vid_language') else None,
+            'dest_languages': self.dest_languages if hasattr(self, 'dest_languages') else None,
+            'are_captions_generated': self.are_captions_generated if hasattr(self, 'are_captions_generated') else False,
+            'captions_type': self.captions_type if hasattr(self, 'captions_type') else 'srt'
+        }
+        self.update_json_file(new_data)
+
+    def generate_captions(self,source_lang: str, dest_langs: list, captions_type="srt"):
         # Your function logic here
         print('Source language:', source_lang)
         print('Destination languages:', dest_langs)
-
-        captions = [{"start":"0:00", "end":"0:05", "text":"Hello, how are you?"}, 
-                    {"start":"0:05", "end":"0:10", "text":"I am fine, thank you."}, 
-                    {"start":"0:10", "end":"0:15", "text":"How about you?"}, 
-                    {"start":"0:15", "end":"0:20", "text":"I am good too."},
-                    {"start":"0:00", "end":"0:05", "text":"Hello, how are you?"}, 
-                    {"start":"0:05", "end":"0:10", "text":"I am fine, thank you."}, 
-                    {"start":"0:10", "end":"0:15", "text":"How about you?"}, 
-                    {"start":"0:15", "end":"0:20", "text":"I am good too."},
-                    {"start":"0:00", "end":"0:05", "text":"Hello, how are you?"}, 
-                    {"start":"0:05", "end":"0:10", "text":"I am fine, thank you."}, 
-                    {"start":"0:10", "end":"0:15", "text":"How about you?"}, 
-                    {"start":"0:15", "end":"0:20", "text":"I am good too."}]
-        
+        self.vid_language = source_lang
+        self.dest_languages = dest_langs
+        self.captions_type = captions_type
         self.video_captioner.generate_captions(media_path=self.vid_path_original,vid_folder=self.vid_folder,
-                                                source_lang=source_lang, dest_langs=dest_langs)
-        return captions
+                                                source_lang=source_lang, dest_langs=dest_langs,captions_type=captions_type)
+        self.are_captions_generated = True
+        return None
 
-    def add_captions(self, dest_langs: list,captions_type="srt"):
+    def get_captions(self,language: str):
+        """
+        Get captions for a specific language
+        """
+        try:
+            captions = parse_captions(f"{self.vid_folder}/{language}.{self.captions_type}")
+            return captions
+        except FileNotFoundError:
+            return []
+    
+    def save_captions(self,captions,language):
+        """
+        Save captions to a file
+        """
+        try:
+            with open(f"{self.vid_folder}/{language}.{self.captions_type}", "w") as file:
+                for i, caption in enumerate(captions):
+                    file.write(f"{i+1}\n")
+                    file.write(f"{caption['start']} --> {caption['end']}\n")
+                    file.write(f"{caption['text']}\n\n")
+        except Exception as e:
+            return str(e)
+
+    def add_captions(self,dest_langs):
         """
         If the generaeted captions are fine, add caption to video
         """
-        self.video_captioner.add_captions(vid_folder=self.vid_folder,vid_extension=self.vid_extension,
-                                        dest_langs=dest_langs, captions_type=captions_type)
-        return True
+        self.dest_languages = dest_langs
+        print("Adding captions to video",self.captions_type)
+        if self.are_captions_generated:
+            self.video_captioner.add_captions(vid_folder=self.vid_folder,vid_extension=self.vid_extension,
+                                            dest_langs=dest_langs, captions_type=self.captions_type)
+            return True
     
--- gui/app.py
+++ gui/app.py
@@ -1,8 +1,10 @@
 import webview
 import webview.menu as wm
 import subprocess
+import platform
 import os
 import shutil
+import time
 from gui.api import VidCaptioAPI
 from gui.utils import find_open_port
 
@@ -11,11 +13,15 @@ class VidCaptioApp:
     """
     App class for the VidCaptio application
     """
-    def __init__(self):
+    def __init__(self,projects_dir):
         self.api = VidCaptioAPI()
-        
-    def upload_video(self,window):
-        file_types = ('Video Files (*.mp4;*.avi;*.mkv)', 'All files (*.*)')
+        self.projects_dir = projects_dir
+
+    def create_project(self,window):
+        """
+        Create a new project and upload a video file
+        """
+        file_types = ('Video Files (*.mp4)', 'All files (*.*)')
         result = window.create_file_dialog(webview.OPEN_DIALOG, allow_multiple=False, file_types=file_types)
         if result:
             original_file_path = result[0]
@@ -24,60 +30,130 @@ class VidCaptioApp:
             # Create a copy of the video file
             base_name = os.path.basename(original_file_path)
             filename,extension = os.path.splitext(base_name)
+
             # Create a new directory in the assets folder with the name of the file
-            new_dir_path = os.path.join(os.path.abspath('assets/'), filename)
-            os.makedirs(new_dir_path, exist_ok=True)
+            self.current_project_dir = os.path.join(self.projects_dir, filename)
+
+            # If the directory already exists, append a number to the filename
+            counter = 1
+            while os.path.exists(self.current_project_dir):
+                filename = f"{filename}_{counter}"
+                self.current_project_dir = os.path.join(self.projects_dir, filename)
+                counter += 1
+
+            os.makedirs(self.current_project_dir, exist_ok=True)
 
             # Create the path to copy the file to
-            copy_file_path = os.path.join(new_dir_path, "video"+extension)
+            copy_file_path = os.path.join(self.current_project_dir, "video"+extension)
 
             # Copy the file
             shutil.copy(original_file_path, copy_file_path)
 
-            self.api.set_video_data(vid_path=original_file_path,vid_folder=f"assets/{filename}",vid_extension=extension)
+            self.api.set_video_data(vid_path=original_file_path,vid_folder=os.path.join(self.projects_dir,filename),
+                                    vid_extension=extension)
             # Send the URL of the video file to the loadVideo function
             url = f'http://localhost:{self.port}/{filename}/{"video"+extension}'
             window.evaluate_js(f'loadVideo("{url}")')
+
+            self.save_project()
             return copy_file_path
         else:
             print('No video selected')
             return None
 
-    def save_video(self):
+    def save_project(self):
+        self.api.save_project_data()
         print('Save video clicked')
 
     def menu_action(self):
         print('Menu item clicked')
 
+    def get_recent_projects(self):
+        projects = [d for d in os.listdir(self.projects_dir) if os.path.isdir(os.path.join(self.projects_dir, d))]
+        return projects
+
+    def open_project(self,window):
+
+        project_folders =self.get_recent_projects()
+
+        window.evaluate_js(f'openProject({project_folders})')
+        while True:
+            result = window.evaluate_js('selectedProject')
+            if result is not None:
+                break
+            time.sleep(0.1)  # Wait a bit before polling again to reduce CPU usage
+        if result:
+            self.current_project_dir = os.path.join(self.projects_dir,result)
+            # Open the project...
+            self.api.load_project_data(self.current_project_dir)
+
+            vid_path = os.path.join(result, "video.mp4")
+            
+            vid_path = vid_path.replace('\\','/')
+            # Send the URL of the video file to the loadVideo function
+            url = f'http://localhost:{self.port}/{vid_path}'
+            window.evaluate_js(f'loadVideo("{url}")')
+            window.evaluate_js(f'loadCaptions("en")')
+            are_captions_generated_js = str(self.api.are_captions_generated).lower()
+            window.evaluate_js(f'loadProject({self.api.dest_languages},{self.api.captions_type},{are_captions_generated_js})')
+    
+    def reveal_project_location(self):
+        file_loc = os.path.join(self.api.vid_folder,"video.mp4")
+        # Open the directory of the video file
+        if platform.system() == 'Windows':
+            os.startfile(os.path.dirname(file_loc))
+        elif platform.system() == 'Darwin':  # macOS
+            subprocess.run(['open', os.path.dirname(file_loc)])
+        else:  # Linux
+            subprocess.run(['xdg-open', os.path.dirname(file_loc)])
+
+    def reveal_file_location(self,file_name):
+        try:
+            file_loc = os.path.join(self.api.vid_folder,file_name)
+            # Open the video file
+            if platform.system() == 'Windows':
+                os.startfile(file_loc)
+            elif platform.system() == 'Darwin':  # macOS
+                subprocess.run(['open', file_loc])
+            else:  # Linux
+                subprocess.run(['xdg-open', file_loc])
+        except:
+            self.reveal_project_location()
+
+    def about_app(self):
+        webview.create_window('About VidCaptio', 'gui/about.html')
     def start(self):
         """
         start the VidCaptio app
         """
-        window = webview.create_window('VidCaptio', 'gui/index.html', js_api=self.api)
+        self.window = webview.create_window('VidCaptio', 'gui/index.html', js_api=self.api)
 
+        self.window.events.closing += self.on_close
         self.port = find_open_port(8000, 8100)
 
         if self.port is None:
             print('No open port found')
         else:
-            server = subprocess.Popen(['python', '-m', 'http.server', str(self.port)], cwd='assets')
+            server = subprocess.Popen(['python', '-m', 'http.server', str(self.port)], cwd=self.projects_dir)
 
         menu_items = [
             wm.Menu('File', [
-                wm.MenuAction('Upload video',  lambda: self.upload_video(window)),
-                wm.MenuAction('Save video', self.save_video),
+                wm.MenuAction('New Project',  lambda: self.create_project(self.window)),
+                wm.MenuAction('Open recent Project', lambda: self.open_project(self.window)),
+                wm.MenuAction('Save Project', lambda: self.save_project()),
             ]),
-            wm.Menu('Edit', [
-                wm.MenuAction('Cut', self.menu_action),
-                wm.MenuAction('Copy', self.menu_action),
-                wm.MenuAction('Paste', self.menu_action),
+            wm.Menu('Open', [
+                wm.MenuAction('Project folder',  lambda: self.reveal_project_location()),
+                wm.MenuAction('Captioned video',  lambda: self.reveal_file_location("captioned.mp4")),
+                wm.MenuAction('Transcript',  lambda: self.reveal_location("transcript.json")),
             ]),
             wm.Menu('About',[
-                wm.MenuAction('About', self.menu_action)
+                wm.MenuAction('About', lambda: self.about_app())
             ])
         ]
 
         webview.start(menu=menu_items)
 
     def on_close(self):
+        self.api.save_project_data()
         print('Window closed')
